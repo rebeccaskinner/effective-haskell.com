4d6191fd5be7961d97c68acaef349eaa9faed05b
Daniel Perkins II <daniel.perkins99@gmail.com>
2023-04-26 22:04:26 -0500

    Adds config changes in default environment file.

--- builder/default.nix
+++ builder/default.nix
@@ -4,6 +4,12 @@
 , returnShellEnv ? false
 }:
 let
+  system = "x86_64";
+  nixpkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/release-21.05.tar.gz";
+    sha256 = "12q00nbd7fb812zchbcnmdg3pw45qhxm74hgpjmshc2dfmgkjh4n";
+  }) { system = system; };
+  pkgs = nixpkgs.pkgs;
   builder =
     hsPkgs.developPackage {
       name = "effective-haskell.com";
@@ -24,3 +30,9 @@ in builder.overrideAttrs (old: {
   LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
   LC_ALL = "C.UTF-8";
 })
+
+
+
+
+
+
